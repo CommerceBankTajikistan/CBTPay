52e23f2380e42fa631d5aaa27ec9f3e3dfbf1b3d
Azimov Daler <daler445@gmail.com>
2020-06-04 23:19:30 +0500

    Update v. 2.4.1
    Bug fixes and code improvement

--- php/CBTPay.php
+++ php/CBTPay.php
@@ -1,251 +1,256 @@
 <?php
+
 /**
  * CBTPay
  * Класс для взаимодействия партнера с интернет-эквайрингом
  * @link https://github.com/CommerceBankTajikistan
  * @copyright ОАО «Коммерцбанк Таджикистана»
- * @version 2.4
- * @since 2020/01/20
+ * @version 2.4.1
+ * @since 2020/06/04
  */
 class CBTPay
 {
-    private $aqId = 0;
-    private $id = "";
-    private $idPrefix = "";
-    private $amount = 0.0;
-    private $currency = "TJS";
-    private $orderDescription = "";
-    private $returnUrl = "";
-    private $failUrl = "";
-    private $callbackUrl = "";
-    private $login = "";
-    private $password = "";
-    private $privateSecurityKey = "";
-    private $token = "";
+	private $aqId = 0;
+	private $id = '';
+	private $idPrefix = '';
+	private $amount = 0.0;
+	private $currency = 'TJS';
+	private $orderDescription = '';
+	private $returnUrl = '';
+	private $failUrl = '';
+	private $callbackUrl = '';
+	private $login = '';
+	private $password = '';
+	private $privateSecurityKey = '';
+	private $token = '';
 
-    /**
-     * @param int $aqId
-     */
-    public function setAqId($aqId)
-    {
-        $this->aqId = $aqId;
-    }
+	/**
+	 * @param int $aqId
+	 */
+	public function setAqId($aqId)
+	{
+		$this->aqId = $aqId;
+	}
 
-    /**
-     * @param $id
-     */
-    public function setId($id)
-    {
-        if (sizeof($this->idPrefix) === 0) {
-            echo "Set id prefix first";
-            exit;
-        }
-        if (strlen($this->idPrefix . $id) > 21) {
-            echo "Length of ID is too long";
-            exit;
-        }
-        $this->id = $id;
-    }
+	/**
+	 * @param $id
+	 */
+	public function setId($id)
+	{
+		if ($this->idPrefix === '') {
+			echo 'Set id prefix first';
+			exit;
+		}
+		if (strlen($this->idPrefix . $id) > 21) {
+			echo 'Length of the ID is too long';
+			exit;
+		}
+		$this->id = $this->idPrefix . $id;
+	}
 
-    /**
-     * @param string $idPrefix
-     */
-    public function setIdPrefix($idPrefix)
-    {
-        $this->idPrefix = $idPrefix;
-    }
+	/**
+	 * @param string $idPrefix
+	 */
+	public function setIdPrefix($idPrefix)
+	{
+		$this->idPrefix = $idPrefix;
+	}
 
-    /**
-     * @param double $amount
-     */
-    public function setAmount($amount)
-    {
-        $this->amount = $amount;
-    }
+	/**
+	 * @param double $amount
+	 */
+	public function setAmount($amount)
+	{
+		$this->amount = $amount;
+	}
 
-    /**
-     * @param string $currency
-     */
-    public function setCurrency($currency)
-    {
-        $this->currency = $currency;
-    }
+	/**
+	 * @param string $currency
+	 */
+	public function setCurrency($currency)
+	{
+		$this->currency = $currency;
+	}
 
-    /**
-     * @param string $orderDescription
-     */
-    public function setOrderDescription($orderDescription)
-    {
-        $this->orderDescription = $orderDescription;
-    }
+	/**
+	 * @param string $orderDescription
+	 */
+	public function setOrderDescription($orderDescription)
+	{
+		$this->orderDescription = $orderDescription;
+	}
 
-    /**
-     * @param string $returnUrl
-     */
-    public function setReturnUrl($returnUrl)
-    {
-        $this->returnUrl = $returnUrl;
-    }
+	/**
+	 * @param string $returnUrl
+	 */
+	public function setReturnUrl($returnUrl)
+	{
+		$this->returnUrl = $returnUrl;
+	}
 
-    /**
-     * @param string $failUrl
-     */
-    public function setFailUrl($failUrl)
-    {
-        $this->failUrl = $failUrl;
-    }
+	/**
+	 * @param string $failUrl
+	 */
+	public function setFailUrl($failUrl)
+	{
+		$this->failUrl = $failUrl;
+	}
 
-    /**
-     * @param string $callbackUrl
-     */
-    public function setCallbackUrl($callbackUrl)
-    {
-        $this->callbackUrl = $callbackUrl;
-    }
+	/**
+	 * @param string $callbackUrl
+	 */
+	public function setCallbackUrl($callbackUrl)
+	{
+		$this->callbackUrl = $callbackUrl;
+	}
 
-    /**
-     * @param string $login
-     */
-    public function setLogin($login)
-    {
-        $this->login = $login;
-    }
+	/**
+	 * @param string $login
+	 */
+	public function setLogin($login)
+	{
+		$this->login = $login;
+	}
 
-    /**
-     * @param string $password
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
+	/**
+	 * @param string $password
+	 */
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
 
-    /**
-     * @param string $privateSecurityKey
-     */
-    public function setPrivateSecurityKey($privateSecurityKey)
-    {
-        $this->privateSecurityKey = $privateSecurityKey;
-    }
+	/**
+	 * @param string $privateSecurityKey
+	 */
+	public function setPrivateSecurityKey($privateSecurityKey)
+	{
+		$this->privateSecurityKey = $privateSecurityKey;
+	}
 
-    /**
-     * @param bool $return Возвращать токен или нет
-     * @param bool $regenerate Сгенерировать заново
-     * @return null|string Токен
-     */
-    public function getToken($return = true, $regenerate = false) {
-        if ($regenerate && (sizeof($this->token) > 0)) return $this->token;
-        $this->setToken(
-            $this->id .
-            $this->orderDescription .
-            $this->amount .
-            $this->login .
-            $this->currency .
-            $this->password .
-            $this->privateSecurityKey
-        );
-        return ($return) ? $this->token : null;
-    }
+	/**
+	 * @param bool $return – Возвращать токен или нет
+	 * @param bool $regenerate – Сгенерировать заново
+	 * @return null|string – Токен
+	 */
+	public function getToken($return = true, $regenerate = false)
+	{
+		if ($regenerate && ($this->token !== '')) {
+			return $this->token;
+		}
+		$this->setToken(
+			$this->id .
+			$this->orderDescription .
+			$this->amount .
+			$this->login .
+			$this->currency .
+			$this->password .
+			$this->privateSecurityKey
+		);
+		return ($return) ? $this->token : null;
+	}
 
-    /**
-     * @param $data string Token data
-     */
-    private function setToken($data) {
-        $this->token = hash_hmac("sha1", $data, $this->privateSecurityKey, $raw_output = false);
-    }
+	/**
+	 * @param $data string Token data
+	 */
+	private function setToken($data)
+	{
+		$this->token = hash_hmac('sha1', $data, $this->privateSecurityKey, $raw_output = false);
+	}
 
-    /**
-     * @return int
-     */
-    public function getAqId()
-    {
-        return $this->aqId;
-    }
+	/**
+	 * @return int
+	 */
+	public function getAqId()
+	{
+		return $this->aqId;
+	}
 
-    /**
-     * @return string
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * @return string
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-    /**
-     * @return string
-     */
-    public function getIdPrefix()
-    {
-        return $this->idPrefix;
-    }
+	/**
+	 * @return string
+	 */
+	public function getIdPrefix()
+	{
+		return $this->idPrefix;
+	}
 
-    /**
-     * @return float
-     */
-    public function getAmount()
-    {
-        return $this->amount;
-    }
+	/**
+	 * @return float
+	 */
+	public function getAmount()
+	{
+		return $this->amount;
+	}
 
-    /**
-     * @return string
-     */
-    public function getCurrency()
-    {
-        return $this->currency;
-    }
+	/**
+	 * @return string
+	 */
+	public function getCurrency()
+	{
+		return $this->currency;
+	}
 
-    /**
-     * @return string
-     */
-    public function getOrderDescription()
-    {
-        return $this->orderDescription;
-    }
+	/**
+	 * @return string
+	 */
+	public function getOrderDescription()
+	{
+		return $this->orderDescription;
+	}
 
-    /**
-     * @return string
-     */
-    public function getReturnUrl()
-    {
-        return $this->returnUrl;
-    }
+	/**
+	 * @return string
+	 */
+	public function getReturnUrl()
+	{
+		return $this->returnUrl;
+	}
 
-    /**
-     * @return string
-     */
-    public function getFailUrl()
-    {
-        return $this->failUrl;
-    }
+	/**
+	 * @return string
+	 */
+	public function getFailUrl()
+	{
+		return $this->failUrl;
+	}
 
-    /**
-     * @return string
-     */
-    public function getCallbackUrl()
-    {
-        return $this->callbackUrl;
-    }
+	/**
+	 * @return string
+	 */
+	public function getCallbackUrl()
+	{
+		return $this->callbackUrl;
+	}
 
-    /**
-     * @return string
-     */
-    public function getLogin()
-    {
-        return $this->login;
-    }
+	/**
+	 * @return string
+	 */
+	public function getLogin()
+	{
+		return $this->login;
+	}
 
-    /**
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
+	/**
+	 * @return string
+	 */
+	public function getPassword()
+	{
+		return $this->password;
+	}
 
-    /**
-     * @return string
-     */
-    public function getPrivateSecurityKey()
-    {
-        return $this->privateSecurityKey;
-    }
+	/**
+	 * @return string
+	 */
+	public function getPrivateSecurityKey()
+	{
+		return $this->privateSecurityKey;
+	}
 }
--- php/callback.php
+++ php/callback.php
@@ -2,38 +2,46 @@
 /**
  * Пример обработки колбэка, получения статуса платежа
  */
-if (isset($_GET['id']) && $_GET['id'] != "") {
-    require_once("CBTPay.php");
-    $id = $_GET['id'];
+if (isset($_GET['id']) && $_GET['id'] !== '') {
+	require_once('CBTPay.php');
+	$id = $_GET['id'];
+	if (class_exists('CBTPay')) {
 
-    $cbt = new CBTPay();
 
-    // Аутенфикация
-    $cbt->setAqId(1);
-    $cbt->setLogin("test");
-    $cbt->setPassword("testPassword");
-    $cbt->setPrivateSecurityKey("testPrivateSecurityKey");
+		$cbt = new CBTPay();
 
-    $cbt->setIdPrefix("TST");
-    $cbt->setId($id);
+		$cbt->setAqId(1);
+		$cbt->setLogin('test');
+		$cbt->setPassword('testPassword');
+		$cbt->setPrivateSecurityKey('testPrivateSecurityKey');
 
-    $curl = curl_init();
+		$cbt->setIdPrefix('TST');
+		$cbt->setId($id);
 
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://pay.cbt.tj/status/".$cbt->getAqId()."/".$cbt->getId()."/".$cbt->getToken(true)."/",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 2,
-        CURLOPT_TIMEOUT => 10,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
+		$curl = curl_init();
 
-    $response = json_decode(curl_exec($curl));
-    curl_close($curl);
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://pay.cbt.tj/status/' . $cbt->getAqId() . '/' . $cbt->getId() . '/' . $cbt->getToken(true) . '/',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 2,
+			CURLOPT_TIMEOUT => 10,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+		));
 
-    echo $response->status_code;
+		$response = json_decode(curl_exec($curl), true);
+		curl_close($curl);
+
+		if ($response !== false) {
+			echo $response['status_code'];
+		} else {
+			trigger_error('Broken response', E_USER_NOTICE);
+		}
+	} else {
+		trigger_error('CBTPay class not found', E_USER_ERROR);
+	}
 } else {
-    echo "ID is not provided";
+	trigger_error('ID is not provided', E_USER_NOTICE);
 }
